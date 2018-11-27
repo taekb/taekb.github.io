35f2382606159f02c1b85cb4483b9257335935a9
joyfuldanny@gmail.com <taekb>
2018-11-27 21:05:09 +0000

    colab -> github

--- hw4/index.js
+++ hw4/index.js
@@ -8,11 +8,11 @@ const HOSTED_URLS = {
 
 const examples = {
   'example1':
-      'I suppose you have heard of the handsome letter Mr . Frank Churchill has written to Mrs . Weston?',
+      'test',
   'example2':
-      'So the little Gingerbread Boy jumped on the fox tail, and the fox began to swim the river.',
+      'test',
   'example3':
-      'Well, Honor is the subiect of my Story: I cannot tell, what you and other men Thinke of this life: But for my single selfe, I had as liefe not be, as liue to be In awe of such a Thing, as I my selfe.'     
+      'test'
 };
 
 function status(statusText) {
