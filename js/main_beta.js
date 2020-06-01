1f5e24b0e087a700d584d655fcd93513e2d081ac
Daniel Jeong <daniel.jeong@columbia.edu>
2020-06-01 18:05:43 +0900

    Update main js

--- js/main_beta.js
+++ js/main_beta.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
 // Cycle through subheader
 var idx = 0;
 setInterval(function() {
-    $('#subname').delay(5000).fadeOut(1000);
+    $('#subname').delay(2000).fadeOut(1000);
     $('#subname').text(subnames[idx++ % subnames.length]);
     $('#subname').fadeIn(1000);
-}, 1000);
+}, 4100);
 
