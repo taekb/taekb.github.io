942f9df6be4a224f676efecea1180fa1ef8c49b8
Daniel Jeong <daniel.jeong@columbia.edu>
2020-06-01 18:00:52 +0900

    Update main js

--- js/main_beta.js
+++ js/main_beta.js
@@ -2,29 +2,18 @@ var subnames = ['Machine Learning + Genomics',
                 'Machine Learning + Neuroscience',
                 'Machine Learning & AI'];
 
-function show_text() {
-	$('#name').hide();
-	$('#subname').hide();
-
-	$('#name').fadeIn(1000);
-	$('#subname').fadeIn(1750);
-}
-
-function update_text() {
-    for (i = 0; i < 3; i++) {
-        $('#subname').delay(1000).fadeOut(1000);
-        $('#subname').text(subnames[i]);
-        $('#subname').fadeIn(1000);
-    }
-}
-
 // Prepare document
-$(document).ready(show_text());
+$(document).ready(function() {
+    $('#name').hide();
+    $('#subname').hide();
+    $('#name').fadeIn(1000);
+    $('#subname').fadeIn(1750);
+});
 
 // Cycle through subheader
 var idx = 0;
 setInterval(function() {
-    $('#subname').delay(1000).fadeOut(1000);
+    $('#subname').delay(5000).fadeOut(1000);
     $('#subname').text(subnames[idx++ % subnames.length]);
     $('#subname').fadeIn(1000);
 }, 1000);
